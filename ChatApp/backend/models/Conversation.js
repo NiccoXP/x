f58425b74ea918b45421ef68793c6fd8ba82ec69
NiccoXP <adxrsh40@gmail.com>
2026-02-05 17:10:49 +0530

    Initialized

--- ChatApp/backend/models/Conversation.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const conversationSchema = new mongoose.Schema({
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    isAccepted: { type: Boolean, default: false }, // Request Folder Logic
-    vanishMode: { type: Boolean, default: false }
-}, { timestamps: true });
-module.exports = mongoose.model('Conversation', conversationSchema);
--- ChatApp/backend/models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Conversation = require('./Conversation');
-const Message = require('./Message');
-
-module.exports = { User, Conversation, Message };
